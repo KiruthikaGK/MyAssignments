8c2a479cc2479e4e0df46f569f15eda0492de133
KiruthikaGK <114807125+KiruthikaGK@users.noreply.github.com>
2022-10-01 16:19:21 +0530

    Add files via upload

---
+++ day1/Factorial.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int a =1;
+				for (int j=5; j>=1; j--)	{
+					a=j*a;
+					
+				}
+				System.out.println(a);
+				 
+	}
+	}		
+	
+	
